76397dcdfa641424a560cb37ac28e61c144fb889
adnanb-bit <r.adnanbabar@gmail.com>
2025-12-25 18:54:58 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: process.env.GITHUB_PAGES ? "/bulbul-diy-kits/" : "/",
+  base: process.env.GITHUB_PAGES ? "genesis-store-glow" : "/",
   server: {
     host: "::",
     port: 8080,
